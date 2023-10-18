b30ea10359a4f8c2e272464f6b3b327504547c4a
Blag <atejada@gmail.com>
2023-10-18 13:03:16 -0400

    Update Program.cs

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,9 +1,13 @@
+// Import packages
 using RestSharp;
 using System;
 
+// Load .env file
 DotNetEnv.Env.Load();
 DotNetEnv.Env.TraversePath().Load();
 
+
+// Ask via the user via console
 Console.WriteLine("Enter subject:");
 string _subject = System.Console.ReadLine();
 Console.WriteLine("Enter name:");
@@ -12,12 +16,18 @@ Console.WriteLine("Enter email:");
 string _email = System.Console.ReadLine();
 Console.WriteLine("Enter body:");
 string _body = System.Console.ReadLine();
-var client = new RestSharp.RestClient("https://api.nylas.com/send");
+// Create a new Rest Client and call the Nylas API endpoint
+var url = "https://api.us.nylas.com/v3/grants/" + Environment.GetEnvironmentVariable("GRANT_ID") + "/messages/send";
+var client = new RestSharp.RestClient(url);
+// Wait until the connection is done 
 client.Timeout = -1;
+// We want a POST method
 var request = new RestSharp.RestRequest(Method.POST);
+// Adding header and authorization
 request.AddHeader("Content-Type", "application/json");
-request.AddHeader("Authorization", "Bearer " + System.Environment.GetEnvironmentVariable("ACCESS_TOKEN"));
+request.AddHeader("Authorization", "Bearer " + Environment.GetEnvironmentVariable("API_KEY_V3"));
 
+// Formatting the body of our request
 var body = @"{"
     + "\n"
     + $@"    ""body"" : ""{_body}"","
@@ -37,7 +47,12 @@ var body = @"{"
     + @"    ]"
     + "\n"
     + @"}";
+
+// Adding the body as a parameter
 request.AddParameter("application/json", body, ParameterType.RequestBody);
+// We send the request
 IRestResponse response = client.Execute(request);
+// Print the response
 Console.WriteLine(response.Content);
+// Wait for user input before closing the terminal
 Console.Read();
