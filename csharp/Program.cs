823c3d5e158eb74c8d69f3323a662def386702d7
Blag <atejada@gmail.com>
2023-10-19 09:58:39 -0400

    Update Program.cs

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,29 +1,26 @@
 // Import packages
 using RestSharp;
-using System;
 
 // Load .env file
 DotNetEnv.Env.Load();
 DotNetEnv.Env.TraversePath().Load();
 
-
 // Ask via the user via console
 Console.WriteLine("Enter subject:");
-string _subject = System.Console.ReadLine();
+string _subject = Console.ReadLine();
 Console.WriteLine("Enter name:");
-string _name = System.Console.ReadLine();
+string _name = Console.ReadLine();
 Console.WriteLine("Enter email:");
-string _email = System.Console.ReadLine();
+string _email = Console.ReadLine();
 Console.WriteLine("Enter body:");
-string _body = System.Console.ReadLine();
+string _body = Console.ReadLine();
 // Create a new Rest Client and call the Nylas API endpoint
 var url = "https://api.us.nylas.com/v3/grants/" + Environment.GetEnvironmentVariable("GRANT_ID") + "/messages/send";
-var client = new RestSharp.RestClient(url);
-// Wait until the connection is done 
-client.Timeout = -1;
+var client = new RestClient(url);
 // We want a POST method
-var request = new RestSharp.RestRequest(Method.POST);
+var request = new RestRequest();
 // Adding header and authorization
+request.AddHeader("Accept", "application/json");
 request.AddHeader("Content-Type", "application/json");
 request.AddHeader("Authorization", "Bearer " + Environment.GetEnvironmentVariable("API_KEY_V3"));
 
@@ -49,9 +46,9 @@ var body = @"{"
     + @"}";
 
 // Adding the body as a parameter
-request.AddParameter("application/json", body, ParameterType.RequestBody);
+request.AddStringBody(body, ContentType.Json);
 // We send the request
-RestResponse response = client.Execute(request);
+RestResponse response = client.ExecutePost(request);
 // Print the response
 Console.WriteLine(response.Content);
 // Wait for user input before closing the terminal
